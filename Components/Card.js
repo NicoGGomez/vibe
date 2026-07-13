27a0cfc1614bf7e46baac6c9089aa88d705f84c7
NicoGGomez <anelecarg@icloud.com>
2026-07-13 11:41:08 -0300

    Codigo mucho mas limpio

---
+++ Components/Card.js
@@ -0,0 +1,24 @@
+class Card extends HTMLElement {
+    connectedCallback() {
+
+        const id = this.getAttribute("data-id");
+        const nombre = this.getAttribute("nombre");
+        const precio = this.getAttribute("precio");
+        const imagen = this.getAttribute("imagen");
+
+        this.innerHTML = `
+                    <div class="cont card" data-id="${id}">
+                        <img src="Imgs/img-card.png" alt="">
+                        <div class="cont info-card-comp">
+                            <div class="cont info-card">
+                                <p class="texto">${nombre}</p>
+                                <p class="precio">${precio}</p>
+                            </div>
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </div>
+                    </div>
+        `;
+    }
+}
+
+customElements.define("card-comp", Card);
