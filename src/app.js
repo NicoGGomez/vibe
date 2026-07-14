92fbd3bf3349183565f36e6cb6dca5eeeba864be
NicoGGomez <anelecarg@icloud.com>
2026-07-13 23:51:03 -0300

    Subida de archivos para el backend

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, "../public")));
+
+module.exports = app;
