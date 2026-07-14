e144add3fd51d858b93f89451787cfba91dd6e2e
NicoGGomez <anelecarg@icloud.com>
2026-07-14 18:17:25 -0300

    se agrega para poder seleccionar la categoria

--- public/Scripts/categorias-producto.js
+++ public/Scripts/categorias-producto.js
@@ -4,9 +4,18 @@ const cargarCategorias = async () => {
 
     try {
 
-        const respuesta = await fetch("/categorias");
+        const respuesta = await fetch(`https://vibe-n9dy.onrender.com/categorias`);
+
+        if (!respuesta.ok) {
+            throw new Error(`Error ${respuesta.status}`);
+        }
 
         const categorias = await respuesta.json();
+
+        if (!Array.isArray(categorias)) {
+            console.log(categorias);
+            return;
+        }
 
         selectCategorias.innerHTML = "";
 
