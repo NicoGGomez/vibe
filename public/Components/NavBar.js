5f4af3c5e6a54cab53bed51d1a6ff4f793bd7d96
NicoGGomez <anelecarg@icloud.com>
2026-07-14 00:35:15 -0300

    cambio de index

--- public/Components/NavBar.js
+++ public/Components/NavBar.js
@@ -4,7 +4,7 @@ class NavBar extends HTMLElement {
             <nav class="cont">
                 <ul>
                     <a href=""><img src="Imgs/logo-vibe.png" alt=""></a>
-                    <li><a href="Index.html">Home</a></li>
+                    <li><a href="index.html">Home</a></li>
                     <li><a href="Productos.html">Productos</a></li>
                     <li><a href="Nosotros.html">Nosotros</a></li>
                 </ul>
