192ed32a3298e3a37a1f35ad4f369aa482c16f30
NicoGGomez <anelecarg@icloud.com>
2026-07-15 11:47:15 -0300

    cambio de seccion productois para q se muestre la card

--- public/Scripts/productos.js
+++ public/Scripts/productos.js
@@ -1,40 +1,38 @@
-const listaProductos = document.getElementById("lista-productos");
+const contenedor = document.getElementById("contenedor-productos");
 
 const cargarProductos = async () => {
 
     try {
-        const respuesta = await fetch(`https://vibe-n9dy.onrender.com/productos`);
 
-        if (!respuesta.ok) {
-            throw new Error(`Error ${respuesta.status}`);
-        }
+        const respuesta = await fetch("https://vibe-n9dy.onrender.com/productos");
 
         const productos = await respuesta.json();
 
-        if (!Array.isArray(productos)) {
-            console.log(productos);
-            return;
-        }
-
-        listaProductos.innerHTML = "";
+        contenedor.innerHTML = "";
 
         productos.forEach(producto => {
 
-            listaProductos.innerHTML += `
-                <li>
-                    ${producto.nombre}
-                </li>
+            contenedor.innerHTML += `
+                <producto-comp
+                    data-id="${producto.id_producto}"
+                    nombre="${producto.nombre}"
+                    precio="${producto.precio}"
+                    descripcion="${producto.descripcion}"
+                    imagen="${producto.imagen_principal}"
+                    imagenExUno="${producto.imagen_extra_uno}"
+                    imagenExDos="${producto.imagen_extra_dos}"
+                    imagenExTres="${producto.imagen_extra_tres}">
+                </producto-comp>
             `;
 
         });
 
     } catch(error) {
 
-        console.log("Error al cargar productos:", error);
+        console.log(error);
 
     }
 
 };
-
 
 cargarProductos();
