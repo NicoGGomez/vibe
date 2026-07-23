e12363c5edbb516793bd03d8690c1dcaacec5881
NicoGGomez <anelecarg@icloud.com>
2026-07-23 18:37:21 -0300

    se sube cambio en el nav para ver dependiendo el rol

--- public/Components/NavBar.js
+++ public/Components/NavBar.js
@@ -1,12 +1,47 @@
+// class NavBar extends HTMLElement {
+//     connectedCallback() {
+//         this.innerHTML = `
+//             <nav class="cont">
+//                 <ul>
+//                     <a href=""><img src="Imgs/logo-vibe.png" alt=""></a>
+//                     <li><a href="index.html">Home</a></li>
+//                     <li><a href="Productos.html">Productos</a></li>
+//                     <li><a href="Nosotros.html">Nosotros</a></li>
+//                 </ul>
+//             </nav>
+//         `;
+//     }
+// }
+
+// customElements.define("nav-bar", NavBar);
+
+import { getUsuario } from "./auth.js";
+
+const usuario = getUsuario();
+
 class NavBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav class="cont">
                 <ul>
                     <a href=""><img src="Imgs/logo-vibe.png" alt=""></a>
+
                     <li><a href="index.html">Home</a></li>
                     <li><a href="Productos.html">Productos</a></li>
                     <li><a href="Nosotros.html">Nosotros</a></li>
+
+                    ${
+                        !usuario
+                            ? `<li><a href="login.html">Login</a></li>`
+                            : `<li><a href="perfil.html">Perfil</a></li>`
+                    }
+
+                    ${
+                        usuario?.rol === "admin"
+                            ? `<li><a href="cargarProducto.html">Cargar producto</a></li>`
+                            : ""
+                    }
+
                 </ul>
             </nav>
         `;
