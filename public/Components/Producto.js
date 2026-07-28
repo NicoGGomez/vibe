4479194919a789e56237fbf485bf0fb9c4153d69
NicoGGomez <anelecarg@icloud.com>
2026-07-28 12:37:09 -0300

    se acomodo para mostrar la informacion del producto correctamente

--- public/Components/Producto.js
+++ public/Components/Producto.js
@@ -1,47 +1,3 @@
-// class Producto extends HTMLElement {
-//     connectedCallback() {
-
-//         const id = this.getAttribute("data-id");
-//         const nombre = this.getAttribute("nombre");
-//         const precio = this.getAttribute("precio");
-//         const descripcion = this.getAttribute("descripcion");
-//         const imagen = this.getAttribute("imagen");
-//         const imagenExtraUno = this.getAttribute("imagenExUno");
-//         const imagenExtraDos = this.getAttribute("imagenExDos");
-//         const imagenExtraTres = this.getAttribute("imagenExTres");
-
-//         this.innerHTML = `
-//         <div class="cont contenedor-producto" data-id="${id}">
-//             <img class="img-producto-prin" src="${imagen}" alt="">
-//             <div class="cont producto-informacion">
-//                 <div class="producto-texto">
-//                     <div class="informacion-principal">
-//                         <p class="nombre">${nombre}</p>
-//                         <p class="precio">$${precio}</p>
-//                     </div>
-//                     <p>${descripcion}</p>
-//                 </div>
-//                 <div class="cont imagenes">
-//                         ${imagenExtraUno ? `<img src="${imagenExtraUno}" alt="">` : ""}
-//                         ${imagenExtraDos ? `<img src="${imagenExtraDos}" alt="">` : ""}
-//                         ${imagenExtraTres ? `<img src="${imagenExtraTres}" alt="">` : ""}
-//                 </div>
-//                 <div class="cont contenedor-botones">
-//                     <div class="separador separador-producto"></div>
-//                     <div class="cont botones">
-//                         <button>Comprar</button>
-//                         <button>Agregar al carrito</button>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//         `;
-//     }
-// }
-
-// customElements.define("producto-comp", Producto);
-
-
 class Producto extends HTMLElement {
 
     connectedCallback() {
@@ -59,6 +15,11 @@ class Producto extends HTMLElement {
         const imagenExtraDos = this.getAttribute("imagenExDos");
         const imagenExtraTres = this.getAttribute("imagenExTres");
 
+        const hayImagenes =
+                imagenExtraUno ||
+                imagenExtraDos ||
+                imagenExtraTres;
+
         this.innerHTML = `
             <div class="cont contenedor-producto" data-id="${id}">
                 <img class="img-producto-prin" src="${imagen}" alt="">
@@ -74,11 +35,13 @@ class Producto extends HTMLElement {
                         <p>${descripcion}</p>
                     </div>
 
-                    <div class="cont imagenes">
-                        ${imagenExtraUno ? `<img src="${imagenExtraUno}" alt="">` : ""}
-                        ${imagenExtraDos ? `<img src="${imagenExtraDos}" alt="">` : ""}
-                        ${imagenExtraTres ? `<img src="${imagenExtraTres}" alt="">` : ""}
-                    </div>
+                    ${hayImagenes ? `
+                        <div class="cont imagenes">
+                            ${imagenExtraUno ? `<img src="${imagenExtraUno}" alt="">` : ""}
+                            ${imagenExtraDos ? `<img src="${imagenExtraDos}" alt="">` : ""}
+                            ${imagenExtraTres ? `<img src="${imagenExtraTres}" alt="">` : ""}
+                        </div>
+                    ` : ""}
 
                     <div class="cont contenedor-botones">
                         <div class="separador separador-producto"></div>
