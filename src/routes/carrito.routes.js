e2cef601e0419b2869cf2a7720b2ae16036a6302
NicoGGomez <anelecarg@icloud.com>
2026-08-01 01:43:00 -0300

    faltaba corregir ruta tokem middleware

--- src/routes/carrito.routes.js
+++ src/routes/carrito.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const carritoController = require("../controllers/carrito.controller");
-const verificarToken = require("../middlewares/verificarToken");
+const verificarToken = require("../middlewares/auth.middleware");
 
 router.post("/", verificarToken, carritoController.agregarProductoCarrito);
 router.get("/", verificarToken, carritoController.getProductosCarrito);
